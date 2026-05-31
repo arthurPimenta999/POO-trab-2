1faaa1a7d1e9ef33da6a623dd86594b40209ba58
nicolas <nicolas.lqsalles@gmail.com>
2026-05-31 10:03:11 -0300

    basicao de dao

--- src/fatec/poo/control/DaoHotel.java
+++ src/fatec/poo/control/DaoHotel.java
@@ -4,7 +4,11 @@
  */
 package fatec.poo.control;
 
+import fatec.poo.model.Hotel;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -16,4 +20,46 @@ public class DaoHotel {
     public DaoHotel(Connection conn) {
          this.conn = conn;
     }  
+    public Hotel consultar (int ra) {
+        Hotel objHotel = null;         
+       
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("qry");
+        }
+        catch (SQLException ex) { 
+             System.out.println(ex.toString());   
+        }
+        return(objHotel);
+    }    
+     
+    public void inserir(Hotel hotel) {
+        PreparedStatement ps;
+        try {
+             ps = conn.prepareStatement("qry");
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }  
+    
+    public void alterar(Hotel hotel) {
+        PreparedStatement ps;
+        try {
+             ps = conn.prepareStatement("qry");
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
+    
+    public void excluir(Hotel hotel) {
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("qry");
+            
+                      
+            ps.execute(); //Envia a instrução SQL para o SGBD
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
 }
--- src/fatec/poo/control/DaoReserva.java
+++ src/fatec/poo/control/DaoReserva.java
@@ -4,7 +4,10 @@
  */
 package fatec.poo.control;
 
+import fatec.poo.model.Reserva;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -12,8 +15,53 @@ import java.sql.Connection;
  */
 public class DaoReserva {
     private Connection conn;
-    
     public DaoReserva(Connection conn) {
          this.conn = conn;
     }  
+    public void consultar(String s){ 
+        return;
+    }
+    
+    public Reserva consultar (int ra) {
+        Reserva objReserva = null;         
+       
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("qry");
+        }
+        catch (SQLException ex) { 
+             System.out.println(ex.toString());   
+        }
+        return(objReserva);
+    }    
+     
+    public void inserir(Reserva reserva) {
+        PreparedStatement ps;
+        try {
+             ps = conn.prepareStatement("qry");
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }  
+    
+    public void alterar(Reserva reserva) {
+        PreparedStatement ps;
+        try {
+             ps = conn.prepareStatement("qry");
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
+    
+    public void excluir(Reserva reserva) {
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("qry");
+            
+                      
+            ps.execute(); //Envia a instrução SQL para o SGBD
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
 }
